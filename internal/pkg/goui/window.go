0e8ccf0e7c1db400e2e30a46474a9d321fe65dd3
Moustapha Saad <moustapha.saad.abdelhamed@gmail.com>
2019-04-26 21:30:24 +0200

    play with performance

--- internal/pkg/goui/window.go
+++ internal/pkg/goui/window.go
@@ -10,7 +10,7 @@ import (
 
 type Window struct {
 	rect geometry.Rect
-	children []Node
+	children []Circle
 	windowHandle *wingui.Window
 	chain *img.Swapchain
 	waitGroup sync.WaitGroup
@@ -23,7 +23,7 @@ func NewWindow(width, height uint32, title string) (*Window, error) {
 		TopLeft: geometry.Vec2{ X: 0, Y: 0 },
 		BottomRight: geometry.Vec2{ X: float32(width), Y: float32(height) },
 	}
-	res.children = make([]Node, 0)
+	res.children = make([]Circle, 0)
 	res.chain = img.NewSwapchain(width, height)
 
 	handle, err := wingui.CreateWindow(title, width, height, &res)
@@ -41,7 +41,7 @@ func (w *Window) Exec() {
 	}
 }
 
-func (w *Window) ChildAdd(c Node) {
+func (w *Window) ChildAdd(c Circle) {
 	w.children = append(w.children, c)
 }
 
@@ -58,7 +58,7 @@ func (w *Window) Frame() img.Image {
 	// 	iLimit, jLimit := r.EndPixel()
 	// 	for j := jStart; j < buffer.Height && j < jLimit; j++ {
 	// 		for i := iStart; i < buffer.Width && i < iLimit; i++ {
-	// 			c := w.Shade(geometry.Vec2{X: float32(i), Y: float32(j)}).Clamp()
+	// 			c := n.Shade(geometry.Vec2{X: float32(i), Y: float32(j)}).Clamp()
 	// 			buffer.PixelSet(i, j, img.Pixel{
 	// 				R: uint8(float32(255) * c.R),
 	// 				G: uint8(float32(255) * c.G),
@@ -70,45 +70,43 @@ func (w *Window) Frame() img.Image {
 	// }
 
 	//Multi Threaded Object Oriented
-	w.waitGroup.Add(len(w.children))
-	for _, n := range w.children {
-		r := n.Rect()
-		go func() {
-			iStart, jStart := r.BeginPixel()
-			iLimit, jLimit := r.EndPixel()
-			for j := jStart; j < buffer.Height && j < jLimit; j++ {
-				for i := iStart; i < buffer.Width && i < iLimit; i++ {
-					c := w.Shade(geometry.Vec2{X: float32(i), Y: float32(j)}).Clamp()
-					buffer.PixelSet(i, j, img.Pixel{
-						R: uint8(float32(255) * c.R),
-						G: uint8(float32(255) * c.G),
-						B: uint8(float32(255) * c.B),
-						A: uint8(float32(255) * c.A),
-					})
-				}
-			}
-			w.waitGroup.Done()
-		}()
-	}
-	w.waitGroup.Wait()
-
-	// Multi threaded Pixel Oriented
-	// w.waitGroup.Add(int(buffer.Height))
-	// for j := uint32(0); j < buffer.Height; j++ {
-	// 	go func(w *Window, b *img.Image, j uint32){
-	// 		for i := uint32(0); i < b.Width; i++ {
-	// 			c := w.Shade(geometry.Vec2{X: float32(i), Y: float32(j)}).Clamp()
-	// 			b.PixelSet(i, j, img.Pixel{
-	// 				R: uint8(float32(255) * c.R),
-	// 				G: uint8(float32(255) * c.G),
-	// 				B: uint8(float32(255) * c.B),
-	// 				A: uint8(float32(255) * c.A),
-	// 			})
+	// w.waitGroup.Add(len(w.children))
+	// for _, n := range w.children {
+	// 	go func() {
+	// 		r := n.Rect()
+	// 		iStart, jStart := r.BeginPixel()
+	// 		iLimit, jLimit := r.EndPixel()
+	// 		for j := jStart; j < buffer.Height && j < jLimit; j++ {
+	// 			for i := iStart; i < buffer.Width && i < iLimit; i++ {
+	// 				c := n.Shade(geometry.Vec2{X: float32(i), Y: float32(j)}).Clamp()
+	// 				buffer.Pixels[i + j * buffer.Width].B += uint8(float32(255) * c.B)
+	// 				buffer.Pixels[i + j * buffer.Width].G += uint8(float32(255) * c.G)
+	// 				buffer.Pixels[i + j * buffer.Width].R += uint8(float32(255) * c.R)
+	// 				buffer.Pixels[i + j * buffer.Width].A += uint8(float32(255) * c.A)
+	// 			}
 	// 		}
 	// 		w.waitGroup.Done()
-	// 	}(w, &buffer, j)
+	// 	}()
 	// }
 	// w.waitGroup.Wait()
+
+	// Multi threaded Pixel Oriented
+	w.waitGroup.Add(int(buffer.Height))
+	for j := uint32(0); j < buffer.Height; j++ {
+		go func(w *Window, b *img.Image, j uint32){
+			for i := uint32(0); i < b.Width; i++ {
+				c := w.Shade(geometry.Vec2{X: float32(i), Y: float32(j)}).Clamp()
+				b.PixelSet(i, j, img.Pixel{
+					R: uint8(float32(255) * c.R),
+					G: uint8(float32(255) * c.G),
+					B: uint8(float32(255) * c.B),
+					A: uint8(float32(255) * c.A),
+				})
+			}
+			w.waitGroup.Done()
+		}(w, &buffer, j)
+	}
+	w.waitGroup.Wait()
 
 	w.chain.Swap()
 	return w.chain.Front()
