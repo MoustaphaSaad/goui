af5f06957bc2ee6455fe49e1e59864bb009529f8
Moustapha Saad <moustapha.saad.abdelhamed@gmail.com>
2019-04-26 19:22:12 +0200

    add object oriented multi threaded code

--- internal/pkg/geometry/rect.go
+++ internal/pkg/geometry/rect.go
@@ -22,6 +22,22 @@ func (r Rect) Inside(point Vec2) bool {
 			point.Y >= r.TopLeft.Y && point.Y <= r.BottomRight.Y)
 }
 
+func (r Rect) BeginPixel() (x, y uint32) {
+	x = 0
+	if r.TopLeft.X > 0 { x = uint32(r.TopLeft.X) }
+	y = 0
+	if r.TopLeft.Y > 0 { y = uint32(r.TopLeft.Y) }
+	return
+}
+
+func (r Rect) EndPixel() (x, y uint32) {
+	x = 0
+	if r.BottomRight.X > 0 { x = uint32(r.BottomRight.X) }
+	y = 0
+	if r.BottomRight.Y > 0 { y = uint32(r.BottomRight.Y) }
+	return
+}
+
 func UnionRect(a, b Rect) Rect {
 	return Rect{
 		TopLeft: MinVec2(a.TopLeft, b.TopLeft),
--- internal/pkg/goui/window.go
+++ internal/pkg/goui/window.go
@@ -50,23 +50,65 @@ func (w *Window) Frame() img.Image {
 	buffer := w.chain.Back()
 	buffer.Clear()
 	//render
-	// Multi threaded
-	w.waitGroup.Add(int(buffer.Height))
-	for j := uint32(0); j < buffer.Height; j++ {
-		go func(w *Window, b *img.Image, j uint32){
-			for i := uint32(0); i < b.Width; i++ {
-				c := w.Shade(geometry.Vec2{X: float32(i), Y: float32(j)}).Clamp()
-				b.PixelSet(i, j, img.Pixel{
-					R: uint8(float32(255) * c.R),
-					G: uint8(float32(255) * c.G),
-					B: uint8(float32(255) * c.B),
-					A: uint8(float32(255) * c.A),
-				})
+
+	//Single Threaded Object Oriented
+	// for _, n := range w.children {
+	// 	r := n.Rect()
+	// 	iStart, jStart := r.BeginPixel()
+	// 	iLimit, jLimit := r.EndPixel()
+	// 	for j := jStart; j < buffer.Height && j < jLimit; j++ {
+	// 		for i := iStart; i < buffer.Width && i < iLimit; i++ {
+	// 			c := w.Shade(geometry.Vec2{X: float32(i), Y: float32(j)}).Clamp()
+	// 			buffer.PixelSet(i, j, img.Pixel{
+	// 				R: uint8(float32(255) * c.R),
+	// 				G: uint8(float32(255) * c.G),
+	// 				B: uint8(float32(255) * c.B),
+	// 				A: uint8(float32(255) * c.A),
+	// 			})
+	// 		}
+	// 	}
+	// }
+
+	//Multi Threaded Object Oriented
+	w.waitGroup.Add(len(w.children))
+	for _, n := range w.children {
+		r := n.Rect()
+		go func() {
+			iStart, jStart := r.BeginPixel()
+			iLimit, jLimit := r.EndPixel()
+			for j := jStart; j < buffer.Height && j < jLimit; j++ {
+				for i := iStart; i < buffer.Width && i < iLimit; i++ {
+					c := w.Shade(geometry.Vec2{X: float32(i), Y: float32(j)}).Clamp()
+					buffer.PixelSet(i, j, img.Pixel{
+						R: uint8(float32(255) * c.R),
+						G: uint8(float32(255) * c.G),
+						B: uint8(float32(255) * c.B),
+						A: uint8(float32(255) * c.A),
+					})
+				}
 			}
 			w.waitGroup.Done()
-		}(w, &buffer, j)
+		}()
 	}
 	w.waitGroup.Wait()
+
+	// Multi threaded Pixel Oriented
+	// w.waitGroup.Add(int(buffer.Height))
+	// for j := uint32(0); j < buffer.Height; j++ {
+	// 	go func(w *Window, b *img.Image, j uint32){
+	// 		for i := uint32(0); i < b.Width; i++ {
+	// 			c := w.Shade(geometry.Vec2{X: float32(i), Y: float32(j)}).Clamp()
+	// 			b.PixelSet(i, j, img.Pixel{
+	// 				R: uint8(float32(255) * c.R),
+	// 				G: uint8(float32(255) * c.G),
+	// 				B: uint8(float32(255) * c.B),
+	// 				A: uint8(float32(255) * c.A),
+	// 			})
+	// 		}
+	// 		w.waitGroup.Done()
+	// 	}(w, &buffer, j)
+	// }
+	// w.waitGroup.Wait()
 
 	w.chain.Swap()
 	return w.chain.Front()
